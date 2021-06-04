82c050b47a1a6c7ac7979af35b2a4af1e93fab7a
Tom McCauley <tpmccauley@gmail.com>
2021-06-04 16:32:26 +0100

    add data and scripts

---
+++ make_json.py
@@ -0,0 +1,54 @@
+import os
+import json
+import re
+
+events = []
+
+gif_files = [fn for fn in os.listdir('./data') if fn.endswith('.gif')]
+
+for gif in gif_files:
+
+    run_event = gif.split('.')[0].split('_')
+    
+    run = int(re.sub(r"\D", "", run_event[0]))
+    event = int(re.sub(r"\D", "", run_event[1]))
+    
+    events.append({
+        'run': run,
+        'event': event,
+        'file_name': gif
+    })
+    
+json_file_name = 'opal-gifs.json'
+    
+json.dump(
+    events,
+    open(json_file_name, 'w'),
+    sort_keys=True,
+    indent=4
+)
+
+print(
+    f'Results written to {json_file_name}'
+)
+    
+event_stats = {}
+
+for e in events:
+
+    run_event = str(e['run'])+'_'+str(e['event'])
+
+    if run_event in event_stats:
+        event_stats[run_event] += 1
+    else:
+        event_stats[run_event] = 1
+
+print(
+    len(events),
+    'event files'
+)
+
+print(
+    len(event_stats),
+    'unique events'
+)
