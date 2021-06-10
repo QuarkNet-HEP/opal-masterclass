03dc16f2eae8d1f8c243bbb1b7fce28052590751
Tom McCauley <tpmccauley@gmail.com>
2021-06-10 17:03:32 +0100

    more webpage
    * add graphics
    
    * recreate project path in app

--- app/app.py
+++ app/app.py
@@ -46,26 +46,125 @@ def create_app():
             'index.html',
             title='OPAL Masterclass',
         )
+        
+    app.add_url_rule('/', 'index', index)
 
+    
+    @app.route('/intro')
+    def intro():
+        return render_template(
+            'introduction.html',
+            title='Introduction',
+        )
+        
+    app.add_url_rule('/intro', 'intro', intro)
+
+    
+    @app.route('/detector')
+    def detector():
+        return render_template(
+            'detector.html',
+            title='Introduction',
+        )
+
+    app.add_url_rule('/detector', 'detector', detector)
+
+    
+    @app.route('/challenge1')
+    def challenge1():
+        return render_template(
+            'introduction.html',
+            title='Challenge 1',
+        )
+    
+    app.add_url_rule('/challenge1', 'challenge1', challenge1)
+
+    
+    @app.route('/challenge2')
+    def challenge2():
+        return render_template(
+            'introduction.html',
+            title='Challenge 2',
+        )
+    
+    app.add_url_rule('/challenge2', 'challenge2', challenge2)
+
+    
+    @app.route('/challenge3')
+    def challenge3():
+        return render_template(
+            'introduction.html',
+            title='Challenge 3',
+        )
+    
+    app.add_url_rule('/challenge3', 'challenge3', challenge3)
+
+    
+    @app.route('/challenge4')
+    def challenge4():
+        return render_template(
+            'introduction.html',
+            title='Challenge 4',
+        )
+    
+    app.add_url_rule('/challenge4', 'challenge4', challenge4)
+
+    
+    @app.route('/challenge5')
+    def challenge5():
+        return render_template(
+            'introduction.html',
+            title='Challenge 5',
+        )
+    
+    app.add_url_rule('/challenge5', 'challenge5', challenge5)
+
+    
+    @app.route('/others')
+    def others():
+        return render_template(
+            'introduction.html',
+            title='Others',
+        )
+    
+    app.add_url_rule('/others', 'others', others)
+
+    
     @app.route('/Z')
-    def z():
+    def Z():
+        return render_template(
+            'introduction.html',
+            title='Z'
+        )
+    
+    @app.route('/W')
+    def W():
+        return render_template(
+            'introduction.html',
+            title='W'
+        )
+    
+    @app.route('/Zdecays')
+    def Zdecays():
         return render_template(
             'events.html',
             title='Z challenge',
             events=zevents,
         )
 
-    @app.route('/W')
-    def w():
+    @app.route('/Wdecays')
+    def Wdecays():
         return render_template(
             'events.html',
             title='W challenge',
             events=wevents,
         )
+
+
+    app.add_url_rule('/Z', 'Z', Z)
+    app.add_url_rule('/W', 'W', W)
     
-    app.add_url_rule('/', 'index', index)
-    
-    app.add_url_rule('/Z', 'Z', z)
-    app.add_url_rule('/W', 'W', w)
+    app.add_url_rule('/Zdecays', 'Zdecays', Zdecays)
+    app.add_url_rule('/Wdecays', 'Wdecays', Wdecays)
     
     return app
